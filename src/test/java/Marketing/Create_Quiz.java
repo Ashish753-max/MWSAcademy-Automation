2483461c7a9058ead257ff2b992c09c20be8fa80
Ashish.rai <ashishappnox1@gmail.com>
2026-06-01 11:44:25 +0530

    Implemented automation for the Quiz section in the marketing

--- src/test/java/Marketing/Create_Quiz.java
+++ src/test/java/Marketing/Create_Quiz.java
@@ -1,9 +1,79 @@
 package Marketing;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Create_Quiz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+ChromeDriver driver = new ChromeDriver();
+        
+        // maximize the window
+        driver.manage().window().maximize();
+        
+        // open the website 
+        driver.get("https://mwstraining.com/");
+        
+        // enter mail
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//input[@placeholder='Enter Email']"))).sendKeys("ashishappnox1@gmail.com");
+        
+        // enter password
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("//input[@placeholder='••••••••']"))).sendKeys("Ashish@567");
+		
+				// click on login button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[1]/div/div/div[1]/div/div/form/div[3]/button"))).click();
+		
+		// click on marketing section
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[1]/div[2]/div[1]/aside/nav/div[7]/button"))).click();
+		
+		// click on the edit section of the created module
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"admin-scroll-container\"]/div/div/main/div/div/div/div[2]/div/div/table/tbody/tr[1]/td[5]/div/button[2]"))).click();
+		
+		// click on add quiz 
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"admin-scroll-container\"]/div/div/div[2]/div/aside/div[2]/div[3]/div[1]/button"))).click();
+		
+		//enter the title 
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[1]/div[1]/div[1]/input"))).sendKeys("Machine Learning");
+		
+		//Enter the description
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[1]/div[1]/div[2]/textarea"))).sendKeys("This quiz is designed to test your knowledge of machine learning concepts, algorithms, and applications. It covers a range of topics including supervised and unsupervised learning, neural networks, and more. Good luck!");
+		
+		// Enter the question
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[1]/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/input"))).sendKeys("What is the primary goal of machine learning?");
+		
+		// Enter the first options
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[1]/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/input"))).sendKeys("To enable computers to learn from data and make predictions or decisions without being explicitly programmed.");
+		
+		// Enter the second options
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[1]/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/input"))).sendKeys("To create complex algorithms that can solve any problem.");
+		
+		// Enter the third options
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[1]/div[2]/div[2]/div/div/div[2]/div[2]/div[3]/input"))).sendKeys("To replace human intelligence with artificial intelligence.");
+		
+		// Enter the fourth options
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[1]/div[2]/div[2]/div/div/div[2]/div[2]/div[4]/input"))).sendKeys("To develop software that can perform any task without human intervention.");
+		
+		
+		//click on publish button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[2]/div/button[2]"))).click();
 
 	}
 
--- src/test/java/Marketing/Delete_Quiz.java
+++ src/test/java/Marketing/Delete_Quiz.java
@@ -1,9 +1,68 @@
 package Marketing;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Delete_Quiz {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		
+ChromeDriver driver = new ChromeDriver();
+        
+        // maximize the window
+        driver.manage().window().maximize();
+        
+        // open the website 
+        driver.get("https://mwstraining.com/");
+        
+        // enter mail
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//input[@placeholder='Enter Email']"))).sendKeys("ashishappnox1@gmail.com");
+        
+        // enter password
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("//input[@placeholder='••••••••']"))).sendKeys("Ashish@567");
+		
+				// click on login button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[1]/div/div/div[1]/div/div/form/div[3]/button"))).click();
+		
+		// click on marketing section
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[1]/div[2]/div[1]/aside/nav/div[7]/button"))).click();
+		
+		// click on the edit section of the created module
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"admin-scroll-container\"]/div/div/main/div/div/div/div[2]/div/div/table/tbody/tr[1]/td[5]/div/button[2]"))).click();
+		
+		// create Actions object
+        Actions actions = new Actions(driver);
+
+        // locate the element where hover is required
+        WebElement hoverElement = wait.until(
+            ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//*[@id=\"admin-scroll-container\"]/div/div/div[2]/div/aside/div[2]/div[3]/button")
+            )
+        );
+
+        // hover on the element
+        actions.moveToElement(hoverElement).perform();
+        Thread.sleep(2000);
+        
+        // click on delete button
+        wait.until(ExpectedConditions.elementToBeClickable(
+			By.xpath("//*[@id=\"admin-scroll-container\"]/div/div/div[2]/div/aside/div[2]/div[3]/button/div/div[2]/div[2]")
+		)).click();
+        
+        // click on confirm delete button
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div[2]/button[2]"))).click();
+        
 
 		
 		
