28c891590d85b4c3e401cea0d50ea9c502fcc32d
Ashish.rai <ashishappnox1@gmail.com>
2026-06-03 08:51:32 +0530

    Implemented automation for company module

--- src/test/java/Company/Create_Company.java
+++ src/test/java/Company/Create_Company.java
@@ -1,10 +1,57 @@
 package Company;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Create_Company {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+ChromeDriver driver = new ChromeDriver();
+        
+        // maximize the window
+        driver.manage().window().maximize();
+        
+        // open the website 
+        driver.get("https://mwstraining.com/");
+        
+        // enter mail
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//input[@placeholder='Enter Email']"))).sendKeys("ashishappnox1@gmail.com");
+        
+        // enter password
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("//input[@placeholder='••••••••']"))).sendKeys("Ashish@567");
+		
+				// click on login button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[1]/div/div/div[1]/div/div/form/div[3]/button"))).click();
+		
+		// click on Companies section
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[1]/div[2]/div[1]/aside/nav/div[5]/button"))).click();
+		
+		// click on create company button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"admin-scroll-container\"]/div/div/header/div[2]/button"))).click();
+		
+		// enter company name
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[1]/div[2]/div[1]/input"))).sendKeys("Open Code");
+		
+		// enter company description
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[1]/div[2]/div[2]/div/div/div[2]/div[1]"))).sendKeys("Open Code is a software development company that specializes in creating innovative solutions for businesses of all sizes. With a team of experienced developers and designers, Open Code delivers high-quality software products that meet the unique needs of its clients. The company is committed to providing exceptional customer service and building long-term relationships with its clients.");
+		
+		//enter product name
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[2]/div[2]/div/div/input"))).sendKeys("Open Code Product");
+		
+		// click on save button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[3]/button[2]"))).click();
 	}
 
 }
