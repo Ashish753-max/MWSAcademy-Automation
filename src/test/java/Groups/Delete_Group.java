5e083b4357c92577fec555efca8f268d882c69b1
Ashish.rai <ashishappnox1@gmail.com>
2026-06-29 15:51:36 +0530

    Implemented automation for the group section

---
+++ src/test/java/Groups/Delete_Group.java
@@ -0,0 +1,47 @@
+package Groups;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Delete_Group {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+ChromeDriver driver = new ChromeDriver();
+        
+        // maximize the window
+        driver.manage().window().maximize();
+        
+        // open the website 
+        driver.get("https://mwstraining.com/");
+        
+        // enter mail
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//input[@placeholder='Enter Email']"))).sendKeys("ashishappnox1@gmail.com");
+        
+        // enter password
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("//input[@placeholder='••••••••']"))).sendKeys("Ashish@567");
+		
+				// click on login button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[1]/div/div/div[1]/div/div/form/div[2]/button"))).click();
+		
+		// click on Groups section
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[1]/div[2]/div[1]/aside/nav/div[5]/button"))).click();
+		
+		// click on delete button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"admin-scroll-container\"]/div/div/main/div/div/div/div[1]/div/div/div/table/tbody/tr[1]/td[4]/div/button[3]"))).click();
+		
+		// click on confirm delete button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div[2]/button[2]"))).click();
+
+	}
+
+}
