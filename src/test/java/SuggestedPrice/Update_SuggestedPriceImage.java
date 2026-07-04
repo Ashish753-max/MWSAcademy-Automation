78256a15fc39cb0ba1f22b449441e4abde3dbeaa
Ashish.rai <ashishappnox1@gmail.com>
2026-07-04 10:13:07 +0530

    Make changes in automation for the company and product

--- src/test/java/SuggestedPrice/Update_SuggestedPriceImage.java
+++ src/test/java/SuggestedPrice/Update_SuggestedPriceImage.java
@@ -1,9 +1,59 @@
 package SuggestedPrice;
 
+import java.io.File;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Update_SuggestedPriceImage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+ChromeDriver driver = new ChromeDriver();
+        
+        // maximize the window
+        driver.manage().window().maximize();
+        
+        // open the website 
+        driver.get("https://mwstraining.com/");
+        
+        // enter mail
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//input[@placeholder='Enter Email']"))).sendKeys("ashishappnox1@gmail.com");
+        
+        // enter password
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("//input[@placeholder='••••••••']"))).sendKeys("Ashish@567");
+		
+				// click on login button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[1]/div/div/div[1]/div/div/form/div[2]/button"))).click();
+		
+		// click on suggested price section
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[1]/div[2]/div[1]/aside/nav/div[8]/button"))).click();
+		
+		// click on edit button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"admin-scroll-container\"]/div/div/main/div/div/div/div[1]/div/div/div/table/tbody/tr/td[5]/div/button[2]"))).click();
+			
+			// click on remove image button
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[1]/div[4]/div[2]/button"))).click();
+			
+			WebElement fileInput = wait.until(
+		            ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[type='file']"))
+		        );
+
+		        // 3. Provide the absolute path to your image
+		        File file = new File("C:\\Users\\user\\Downloads\\images (1).png");
+		        fileInput.sendKeys(file.getAbsolutePath());
+			
+			// click on confirm remove button
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[2]/div[2]/form/div[2]/button[2]"))).click();
 
 	}
 
